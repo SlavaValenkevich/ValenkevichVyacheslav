d2f97701eb9c5c21b211faa2ef82fa89372f70b9
Vyacheclav Valenkevich <slava2232162012@gmail.com>
2019-04-20 10:40:40 +0300

    Реализована функция encrypt_caesar()

--- lab1/caesar.py
+++ lab1/caesar.py
@@ -11,7 +11,29 @@ def encrypt_caesar(plaintext):
     >>> encrypt_caesar("")
     ''
     """
-    # PUT YOUR CODE HERE
+    ciphertext=""
+    if shift <=26:
+        shift=shift
+    else:
+        shift=shift%26 #остаток от деления
+        print('Смещение=',shift)
+    for i in plaintext:
+        if (ord(i)>=65) and (ord(i)<=90): #ord  - достает код символа в ascii-таблице, здесь проверка на символы A-Z 
+            if ((ord(i)+shift)>=65) and ((ord(i)+shift)<=90): #если символ со смещением укладывается в алфавит A-Z, то
+                ciphertext=ciphertext+chr(ord(i)+shift) #добавляем этот символ в зашифрованную последовательность
+            else:
+                d=int(math.fabs(90-ord(i))) #иначе сделать "круг" 
+                s=64+int(math.fabs((shift-d))) #вычислить новое смещение и прибавить
+                ciphertext=ciphertext+chr(s) #добавить символ в последовательность
+        if (ord(i)>=97) and (ord(i)<=122): #ord  - достает код символа в ascii-таблице, здесь проверка на символы a-z
+            if ((ord(i)+shift)>=97) and ((ord(i)+shift)<=122): #если символ со смещением укладывается в алфавит a-z, то
+                ciphertext=ciphertext+chr(ord(i)+shift) #добавить символ в последовательность
+            else:
+                d=int(math.fabs(122-ord(i))) #иначе делаем круг
+                s=96+int(math.fabs((shift-d))) #вычисляем новое значение
+                ciphertext=ciphertext+chr(s) #добавляем в последовательность
+        if (ord(i)>=44) and (ord(i)<=57):
+            ciphertext+=i
     return ciphertext
 
 
