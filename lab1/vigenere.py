799f0878f6b6e3d16ab939f48827dffbf630d73b
Vyacheclav Valenkevich <slava2232162012@gmail.com>
2019-04-20 10:48:13 +0300

    Реализована функция encrypt_vigenere()

--- lab1/vigenere.py
+++ lab1/vigenere.py
@@ -9,7 +9,42 @@ def encrypt_vigenere(plaintext, keyword):
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    # PUT YOUR CODE HERE
+    ciphertext = ""
+    if len(keyword) < len(plaintext):
+        # целое число раз
+        z = len(plaintext) // len(keyword)
+        keyword *= z
+        if len(plaintext) % len(keyword) != 0:
+            d = len(plaintext) % len(keyword)
+            for h in range(d):
+                # добавление оставшихся символов ключа до длины текста
+                keyword += keyword[h]
+        while len(ciphertext) < len(plaintext):
+            # дальше вычисление смещения для каждого символа относительно символа в ключе
+            for i in range(len(plaintext)):
+                if (ord(keyword[i]) >= 65) and (ord(keyword[i]) <= 90):
+                    # если символ ключа A - Z, то смещение такое
+                    shift = int(math.fabs(65 - ord(keyword[i])))
+                elif(ord(keyword[i]) >= 97) and (ord(keyword[i]) <= 122):
+                    shift = int(math.fabs(97 - ord(keyword[i])))
+                    # если символ a - z, то смещение такое
+                if (ord(plaintext[i]) >= 65) and (ord(plaintext[i]) <= 90):
+                    # дальше логика точно как в шифровании цезаре
+                    if ((ord(plaintext[i]) + shift) >= 65) and ((ord(plaintext[i]) + shift) <= 90):
+                        ciphertext = ciphertext + chr(ord(plaintext[i]) + shift)
+                    else:
+                        d = int(math.fabs(90 - ord(plaintext[i])))
+                        s = 64 + int(math.fabs((shift - d)))
+                        ciphertext = ciphertext + chr(s)
+                    shift = 0
+                if (ord(plaintext[i]) >= 97) and (ord(plaintext[i]) <= 122):
+                    if ((ord(plaintext[i]) + shift) >= 97) and ((ord(plaintext[i]) + shift) <= 122):
+                        ciphertext = ciphertext + chr(ord(plaintext[i]) + shift)
+                    else:
+                        d = int(math.fabs(122 - ord(plaintext[i])))
+                        s = 96 + int(math.fabs((shift - d)))
+                        ciphertext = ciphertext + chr(s)
+                    shift = 0
     return ciphertext
 
 
